7ada016f7cf1c79b555bf8bf557d22158d2444da
sairamarava <sairamrdya@gmail.com>
2024-08-28 18:36:17 +0530

    by sairam testing

--- setup.py
+++ setup.py
@@ -14,34 +14,22 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     url='https://github.com/sairamarava/MITS_AIBIRDS',
     license='GPLv3',
-    install_requires=[            # All necessary packages utilized by our PAMI software
-        'psutil',
-        'pandas',
-        'plotly',
-        'matplotlib',
-        'resource',
-        'validators',
-        'urllib3',
-        'Pillow',
-        'numpy',
-        'sphinx',
-        'sphinx-rtd-theme',
-        'validators',
-        'discord.py',
-        'networkx',
-        'deprecated',
-    ],
+    install_requires=['numpy', 'scipy'],
     extras_require={
         'gpu':  ['cupy', 'pycuda'],
         'spark': ['pyspark'],
         'dev': ['twine', 'setuptools', 'build'],
-        'all': ['cupy', 'pycuda', 'pyspark', 'twine', 'setuptools', 'build']
+        'docs': [ 'sphinx>=3.0.0']
     },
     classifiers=[
-        'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Intended Audience :: Science/Research'
     ],
     python_requires='>=3.5',
 )
